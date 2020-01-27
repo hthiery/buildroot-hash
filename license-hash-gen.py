ba60cc2748ecb6d8ba9d7ed2264466147cb2590d
Heiko Thiery <heiko.thiery@gmail.com>
2020-01-27 05:54:26 +0100

    initial
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

---
+++ license-hash-gen.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+import os
+import json
+import subprocess
+import sys
+
+stats_json = sys.argv[1]
+brpath = sys.argv[2]
+
+def get_info(name, brpath):
+    try:
+        output = subprocess.check_output(
+                    ['make', '{}-show-info'.format(name)], cwd=brpath)
+    except subprocess.CalledProcessError:
+        output = subprocess.check_output(
+                    ['make', 'host-{}-show-info'.format(name)], cwd=brpath)
+    #print(output)
+    info = json.loads(output)
+    return (name, info)
+
+def extract_pkg(name, brpath, source):
+    pkg_dl_path = os.path.join(brpath, 'dl', name)
+    output = subprocess.check_output(
+            ['tar', 'xvf', source, '--strip-components=1'], cwd=pkg_dl_path)
+    #print(output)
+
+def create_hash(name, brpath, license):
+    pkg_dl_path = os.path.join(brpath, 'dl', name)
+    output = subprocess.check_output(
+            ['sha256sum', license], cwd=pkg_dl_path)
+    #print(output)
+    return 'sha256  {}'.format(output)
+
+skip_pkgs = ['android-tools', 'linux-headers', 'imx-gpu-viv', 'qpid-proton'
+        'imx-gpu-g2d']
+with open(stats_json) as f:
+    pkgs = json.load(f)['packages']
+
+    for name in pkgs:
+        pkg = pkgs[name]
+        if pkg['status']['hash-license'][0] == 'ok':
+        #    print('hash-license ok ...')
+            continue
+        if pkg['status']['license'][0] != 'ok':
+        #    print('no license ...')
+            continue
+        if name in skip_pkgs:
+        #    print('skipping ...')
+            continue
+        if name.startswith('xdriver'):
+            continue
+        if pkg['license_files'] is None:
+        #    print('no license file ...')
+            continue
+        #if name.startswith('a') is False:
+        #    continue
+
+        print('#######', name)
+
+        (name, info) = get_info(name, brpath)
+
+        try:
+            if len(info[name]['downloads']) == 0:
+                continue
+        except KeyError:
+            continue
+
+        source = info[name]['downloads'][0]['source']
+        if source.endswith('zip'):
+            continue
+
+        try:
+            output = subprocess.check_output(
+                        ['make', '{}-legal-info'.format(name)],
+                        stderr=subprocess.STDOUT,
+                        cwd=brpath)
+        except subprocess.CalledProcessError:
+            try:
+                output = subprocess.check_output(
+                        ['make', 'host-{}-legal-info'.format(name)],
+                        stderr=subprocess.STDOUT,
+                        cwd=brpath)
+            except subprocess.CalledProcessError:
+                continue
+
+        if output.find('No hash found') == -1:
+            print('hash found')
+            continue
+        else:
+            print('no hash found')
+
+        extract_pkg(name, brpath, source)
+
+        print(pkg)
+        pkg_hash = os.path.join(brpath, pkg['pkg_path'], '{}.hash'.format(name))
+        with open(pkg_hash, "a") as myfile:
+            myfile.write('# locally calculated hash for license file\n')
+            for license in pkg['license_files']:
+                line = create_hash(name, brpath, license)
+                myfile.write(line)
+
+
+
